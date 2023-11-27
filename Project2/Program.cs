2f4e64c7545e030a4fef36de0b30a814e9a69243
Mittaanjali <mitta.anjali@mphasis.com>
2023-11-27 20:43:05 +0530

    Project2

---
+++ Project2/Program.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.SqlServer.Server;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConAppStudentdata
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string path = @"C:\Users\anjal\OneDrive\Desktop\Mphasis(.net)\Rainbow School.txt";
+                string[] files = File.ReadAllLines(path);
+                Console.WriteLine("The Students Data are \n");
+                int index = 1;
+                foreach (string line in files)
+                {
+                    Console.WriteLine($" student {index++} : " + line);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally
+            {
+                Console.WriteLine();
+                Console.Write("All the Students data in the files are displayed: ");
+            }
+            Console.ReadKey();
+        }
+
+
+    }
+
+}
